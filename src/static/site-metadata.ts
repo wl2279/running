3a839e21dc49bddf7d718171377a06a2bfaad8c6
Wanglei <wanglei2279@126.com>
2025-07-19 21:53:13 +0800

    customize

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -15,9 +15,9 @@ const getBasePath = () => {
 };
 
 const data: ISiteMetadataResult = {
-  siteTitle: 'Running Page',
-  siteUrl: 'https://yihong.run',
-  logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
+  siteTitle: '王磊的户外跑',
+  siteUrl: 'https://wanglei2279.vercel.app/',
+  logo: 'https://pica.zhimg.com/fab23f1bb7896e64aeb114a203b30996_l.jpg?source=32738c0c',
   description: 'Personal site and blog',
   navLinks: [
     {
@@ -25,8 +25,8 @@ const data: ISiteMetadataResult = {
       url: `${getBasePath()}/summary`,
     },
     {
-      name: 'Blog',
-      url: 'https://github.com/yihong0618/gitblog',
+      name: 'R.I.P',
+      url: 'https://mp.weixin.qq.com/s?__biz=MzkwODY5MDQ2Nw==&mid=2247519433&idx=1&sn=79dd2aff6f7325f601faba23cabec780&source=41&poc_token=HOWee2ijL8qqFWjE7A-P9nADgxkH03oKmYmx9Fpo',
     },
     {
       name: 'About',
